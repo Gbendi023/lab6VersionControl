76581933c2664ebcfe90680808ec3d5dca54256a
Gabriel Bendix <gabrielbendix@ufl.edu>
2023-10-31 18:17:44 -0400

    this is my first addition to the repo

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-# this is my first gir hub edition 
-print("this is my first :>")
+# this is my first git hub experiement
+#list(str(94879599)) = [9,4,8,.......]
+
+prog = True
+
+while prog:
+    print('would you like to encode or decode a password? ', end='')
+    option = str(input(""))
+
+
+    if option == "encode":
+        print("Please enter your eight digit password: ", end='')
+        elist = int(input(""))
+
+        if elist< 0:
+            print("Incorrect password format. Try again!")
+            continue
+
+        print(f"your encoded value is: {elist+33333333}")
+        continue
+
+    if option == "decode":
+        print("Please enter your eight digit password: ", end='')
+        dlist = int(input(""))
+
+        if dlist< 0:
+            print("Incorrect password format. Try again!")
+            continue
+
+        print(f"your encoded value is: {dlist-33333333}")
+        continue
